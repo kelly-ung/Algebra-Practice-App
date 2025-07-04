4fd7717e4d185d60d411753cc4598377583286eb
Kelly Ung <kellyung@myyahoo.com>
2025-07-04 16:14:48 -0700

    adjust code indentation

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,16 @@
-import bee from '../assets/bee.png';
+import bee from "../assets/bee.png";
 
 // Header of the website with a mascot, title, and description
 const Header = () => {
-    return (
-        <header className="p-10 pl-10 sm:pl-16 md:pl-24 lg:pl-32">
-            <div className="flex items-center gap-3">
-                <img src={bee} alt="Bee" className="w-20 h-auto" />
-                <h1 className="text-4xl font-bold text-bee-brown">Algebee</h1>
-            </div>
-            <p className="text-lg mt-2">Practice Solving Linear Algebra Equations</p>
-        </header>
-    );
-}
+  return (
+    <header className="p-10 pl-10 sm:pl-16 md:pl-24 lg:pl-32">
+      <div className="flex items-center gap-3">
+        <img src={bee} alt="Bee" className="w-20 h-auto" />
+        <h1 className="text-4xl font-bold text-bee-brown">Algebee</h1>
+      </div>
+      <p className="text-lg mt-2">Practice Solving Linear Algebra Equations</p>
+    </header>
+  );
+};
 
 export default Header;
--- client/src/components/LivesDisplay.tsx
+++ client/src/components/LivesDisplay.tsx
@@ -6,18 +6,18 @@ type LivesProps = {
 
 // Display the number of lives left based on wrong answer count
 const LivesDisplay: React.FC<LivesProps> = ({ wrongCount }) => {
-    return (
-        <div className="flex flex-row justify-center gap-2 mb-10">
-            {[0, 1, 2].map((i) => (
-                <img
-                    key={i}
-                    src={honeycomb}
-                    alt="Honeycomb"
-                    className={`w-6 h-auto ${wrongCount > i ? "grayscale" : ""}`} // Apply grayscale effect to honeycomb images based on wrong answer count
-                />
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-row justify-center gap-2 mb-10">
+      {[0, 1, 2].map((i) => (
+        <img
+          key={i}
+          src={honeycomb}
+          alt="Honeycomb"
+          className={`w-6 h-auto ${wrongCount > i ? "grayscale" : ""}`} // Apply grayscale effect to honeycomb images based on wrong answer count
+        />
+      ))}
+    </div>
+  );
+};
 
 export default LivesDisplay;
